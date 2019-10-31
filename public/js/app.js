8517aa2b8fda38b54e1ff2e577ef61a77fbdc44f
Hidalgo Mac <hidalgoponce@icloud.com>
2019-10-31 11:01:01 -0500

    Update

--- public/js/app.js
+++ public/js/app.js
@@ -54667,9 +54667,9 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/freddy/Homestead/codigo/freewalk/resources/js/app.js */"./resources/js/app.js");
-__webpack_require__(/*! /Users/freddy/Homestead/codigo/freewalk/resources/sass/app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! /Users/freddy/Homestead/codigo/freewalk/resources/sass/admin.scss */"./resources/sass/admin.scss");
+__webpack_require__(/*! /Users/hidalgoponce/web/sites/freewalk/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /Users/hidalgoponce/web/sites/freewalk/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /Users/hidalgoponce/web/sites/freewalk/resources/sass/admin.scss */"./resources/sass/admin.scss");
 
 
 /***/ })
